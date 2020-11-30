bde20f2ba17d0a0664684f7efda2d8efb60ec901
Roman Yarovoy <roman@booma.co.il>
2020-11-30 21:27:46 +0200

    add__pwa__rep

---
+++ pwa__weather/src/App.js
@@ -0,0 +1,30 @@
+import React, {useState} from 'react'
+import './App.css';
+import { fetchWeather } from './api/fetchWeather'
+
+function App() {
+  const [query, setQuery] = useState('');
+
+
+  const search = async(e) => {
+    if (e.key === 'Enter') {
+      const data = await fetchWeather(query)
+      console.log(data);
+  }
+}
+
+  return (
+    <div className="main-container">
+      <input
+        type="text"
+        className="search"
+        placeholder="Search..."
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        onKeyPress={search}
+       />
+    </div>
+  );
+}
+
+export default App;
